cefd7a555d355a114e1efd7238f2153ac5ab763b
Rafly Mizalfi <42695564+raflymizalfi@users.noreply.github.com>
2021-06-14 20:32:42 +0700

    Update room.blade.php

--- resources/views/pages/services/components/room.blade.php
+++ resources/views/pages/services/components/room.blade.php
@@ -7,7 +7,7 @@
                         <path d="M51.8388 19.2501V3.11798H42.7054V11.0338L30.5291 0.0727539L0.0881805 27.4709H9.22157V51.8237H24.4409V33.5591H36.6173V51.8237H51.8388V27.4709H60.9722L51.8388 19.2501ZM24.4409 21.3759C24.4409 19.7612 25.0823 18.2127 26.2241 17.0709C27.3658 15.9291 28.9144 15.2877 30.5291 15.2877C32.1438 15.2877 33.6923 15.9291 34.8341 17.0709C35.9758 18.2127 36.6173 19.7612 36.6173 21.3759H24.4409Z" fill="#00C46F" />
                     </svg> All Room
                 </h1>
-                <h6>All Room 
+                <h6>All Room
                     We clean all parts of the room in the house such as bedrooms, bathrooms, living rooms, kitchens, courtyards, and garages.
                 </h6>
             </div>
@@ -23,7 +23,7 @@
                 <h6>We cleaning all side of the room and We also tidy up messy items and provide air freshener so that the room smells fresh</h6>
             </div>
 
-             </div>
+        </div>
         <div class="card my-5">
             <img src="img/bathroom.jpg" alt="">
             <div class="container">
@@ -34,7 +34,7 @@
                 <h6> we clean your bathroom where parts of the room are starting to get dull and we are ready to clean it until it looks like a new room</h6>
             </div>
 
-             </div>
+        </div>
         <div class="card my-5">
             <img src="img/livingroom.jpg" alt="">
             <div class="container">
@@ -46,7 +46,7 @@
             </div>
 
         </div>
-        <nav aria-label="Page navigation example">
+        <!-- <nav aria-label="Page navigation example">
             <ul class="pagination justify-content-center">
                 <li class="page-item ">
                     <a class="page-link black-text" href="#" tabindex="-1" aria-disabled="true">Prev</a>
@@ -58,6 +58,6 @@
                     <a class="page-link" href="#" style="color : #00C46F">next</a>
                 </li>
             </ul>
-        </nav>
+        </nav> -->
     </div>
 </div>
