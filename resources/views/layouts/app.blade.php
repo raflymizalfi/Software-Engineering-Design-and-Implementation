c10074e642aba424a5e8d83cd80bac38659ff4d7
Rafly Mizalfi <42695564+raflymizalfi@users.noreply.github.com>
2021-06-14 14:19:09 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@0;900&display=swap" rel="stylesheet">
     <title>Dailydaily</title>
@@ -28,8 +27,8 @@
             font-size: 24px;
         }
 
-        .fs-20{
-            font-size : 20px;
+        .fs-20 {
+            font-size: 20px;
         }
 
         .fs-15 {
@@ -78,7 +77,12 @@
 
         .card {
             box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+            cursor: pointer;
 
+        }
+
+        .gray-text {
+            color: #C4C4C4;
         }
 
         .pagination-active {
@@ -86,20 +90,32 @@
             color: #00C46F;
         }
 
-        .dropdown-item:hover{
-            background-color : #00C46F;
+        .active-order {
+            color: #00C46F;
+            cursor: pointer;
         }
 
-        .page-link{
+        .dropdown-item:hover {
+            background-color: #00C46F;
+        }
+
+        .page-link {
             border: none;
-            color : #8C8C8C;
+            color: #8C8C8C;
         }
 
-        .order .card{
-            box-shadow : none;
-            border : none;
+        .order .card {
+            box-shadow: none;
+            border: none;
         }
 
+        .active-payment {
+            background-color: #00C46F;
+        }
+
+        .pointer {
+            cursor: pointer;
+        }
     </style>
 </head>
 
@@ -110,8 +126,37 @@
 
     @include("layouts.components.footer")
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+    </script>
+    <script>
+        function activeClass(params) {
+            const arr = ["home", "bedroom", "bathroom", "living", "kitchen", "yard", "garage", "warehouse"]
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i] === params) {
+                    console.log(params)
+                    let nowParams = document.querySelector(`.${params}-icon`)
+                    console.log(nowParams)
+                    nowParams.classList.toggle('active-order')
+                } else {
+                    let remove = document.querySelector(`.${arr[i]}-icon`)
+                    remove.classList.remove('active-order')
+                }
+            }
+        }
+
+
+
+        function activeClassPayment(params) {
+            const cash = document.querySelector('.cash')
+            const bank = document.querySelector('.bank')
+            if (params === 'bank') {
+                bank.classList.toggle('active-payment')
+                cash.classList.remove('active-payment')
+            } else if (params === 'cash') {
+                cash.classList.toggle('active-payment')
+                bank.classList.remove('active-payment')
+            }
+        }
     </script>
 </body>
 
