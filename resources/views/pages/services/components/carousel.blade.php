074e37cfc682e5d2d21c3fc1dd498794f9500761
Rafly Mizalfi <42695564+raflymizalfi@users.noreply.github.com>
2021-06-16 00:57:43 +0700

    update

--- resources/views/pages/services/components/carousel.blade.php
+++ resources/views/pages/services/components/carousel.blade.php
@@ -1,30 +1,30 @@
 <div>
-  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div id="carouselExampleCaptions" class="carousel slide " data-bs-ride="carousel">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="img/maskGroup.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block text-start mb-5">
-          <h5 class="fs-45">Home service</h5>
-          <p class="fs-30">Some representative placeholder content for the first slide.</p>
+    <div class="carousel-inner ">
+      <div class="carousel-item active ">
+        <img src="img/img1.jpeg" class="d-flex-auto w-100 " alt="...">
+        <div class="carousel-caption d-none d-md-block text-start mb-5 bg-gradient-dark text-white">
+          <h5 class="fs-45">Clean</h5>
+          <p class="fs-30">Clean your home very cleanly and neatly</p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="img/kitchen.jpg" class="d-block w-100 .opacity-3" alt="...">
+        <img src="img/img2.jpeg" class="d-block w-100 .opacity-3" alt="...">
         <div class="carousel-caption d-none d-md-block text-start mb-5">
-          <h5 class="fs-45">Kitchen service</h5>
-          <p class="fs-30">Some representative placeholder content for the Second slide.</p>
+          <h5 class="fs-45">Comfort</h5>
+          <p class="fs-30">Make your residence more comfortable so that you and your family can spend time happily</p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="img/hero.jpg" class="d-block w-100" alt="...">
+        <img src="img/img3.jpg" class="d-block w-100" alt="...">
         <div class="carousel-caption d-none d-md-block text-start mb-5">
-          <h5 class="fs-45">All room service</h5>
-          <p class="fs-30">Some representative placeholder content for the Third slide.</p>
+          <h5 class="fs-45">Detail</h5>
+          <p class="fs-30">Clean every corner of your room carefully and in detail</p>
         </div>
       </div>
     </div>
